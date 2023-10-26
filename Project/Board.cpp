e7e79794e263f32b400718206b61677ca2b16d13
Livia-unitbv <livia.toma@student.unitbv.ro>
2023-10-26 17:29:07 +0300

    Added constructor of Board class

--- Project/Board.cpp
+++ Project/Board.cpp
@@ -1,5 +1,6 @@
 #include "Board.cppm"
 
+<<<<<<< HEAD
 namespace twixt {
 	void Board::addPossibleBridges(std::vector<std::vector<std::vector<std::pair<int, int>>>>& possible, int x, int y, twixt::Dot::DotStatus status)
 	{
@@ -10,5 +11,31 @@ namespace twixt {
 				possible[x + pair.first][y + pair.second].push_back({ x,y });
 		}
 	}
+=======
+namespace twixt
+{
+	Board::Board(){}
+
+	Board::Board(uint32_t size)
+	{
+		m_matrixDot.resize(size);
+		for (auto line : m_matrixDot)
+		{
+			line.resize(size);
+		}
+	}
+	Board::Board(const Board & newBoard)
+	{
+		m_possibleBridgesPlayer1 = newBoard.m_possibleBridgesPlayer1;
+		m_possibleBridgesPlayer2 = newBoard.m_possibleBridgesPlayer2;
+		m_matrixDot = newBoard.m_matrixDot;
+	}
+	Board::~Board(){}
+	Board& Board::operator=(const Board & newBoard)
+	{
+		m_possibleBridgesPlayer1 = newBoard.m_possibleBridgesPlayer1;
+		m_possibleBridgesPlayer2 = newBoard.m_possibleBridgesPlayer2;
+		m_matrixDot = newBoard.m_matrixDot;
+>>>>>>> bcec62e (Added constructor of Board class)
 	}
 }
--- Project/Board.cppm
+++ Project/Board.cppm
@@ -1,16 +1,24 @@
 export module Board;
 
-export import Dot;
+export import Dot.cppm;
 
 import <vector>;
 
 namespace twixt {
 	export class Board {
-		// Constructor goes here
+	public:
+		// Constructor
+		Board();
+		Board(uint32_t size);
+		Board(const Board& newBoard);
+		~Board();
+		Board& operator=(const Board& newBoard);
+
 	private:
 		std::vector<std::vector<std::vector<std::pair<int, int>>>> m_possibleBridgesPlayer1, m_possibleBridgesPlayer2;
 		std::vector<std::vector<Dot>> m_matrixDot;
 	public:
 		void addPossibleBridges(std::vector<std::vector<std::vector<std::pair<int, int>>>>& possible, int x, int y, twixt::Dot::DotStatus status);
 	};
+
 }

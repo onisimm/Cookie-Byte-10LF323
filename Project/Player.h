3faeb89d2b095cc069b9937bd77e720337f314b7
Elenaaa19 <elena.susca@student.unitbv.ro>
2023-11-21 12:39:30 +0200

    Added move constructor in Player class

--- Project/Player.h
+++ Project/Player.h
@@ -1,11 +1,52 @@
-
+﻿
 #include <cstdint>
 #include <iostream>
 #include "Board.h"
 #include "Dot.h"
 
 
+
 namespace twixt {
+	class Player {
+	public:
+		//enum
+		enum class Color : uint8_t {
+			Red,
+			Black
+		};
+
+		// Constructors
+		Player(std::string name, Color color);
+		Player(Player&& other) noexcept; // move constructor
+
+		// Destructor
+		~Player();
+
+		// Getters
+		std::string getName() const;
+		Color getColor() const;
+
+		// Setters
+		void setName(std::string);
+		void setColor(Color);
+
+		void turn(Board& board) const;
+
+		friend std::istream& operator>>(std::istream& in, Player& player);
+
+	private:
+		std::string m_name;
+		Color m_color;
+	};
+}
+
+// Implementarea move constructorului
+ twixt::Player::Player(Player&& other) noexcept: m_name(std::move(other.m_name)),m_color(std::move(other.m_color)) {
+	// Setăm membrii altui obiect pe nullptr sau valorile implicite, în funcție de tipul lor.
+	other.m_name.clear();
+	// other.m_color rămâne cu o valoare validă, dar nu ar trebui accesată în continuare în obiectul sursă.
+}
+/*namespace twixt {
 	class Player {
 	public:
 		//enum
@@ -37,4 +78,4 @@ namespace twixt {
 		std::string m_name;
 		Color m_color;
 	};
-}
+}*/

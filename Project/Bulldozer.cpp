57b7f25c46ebe6442cf80a0663eea3d50fc4aa76
Livia-unitbv <livia.toma@student.unitbv.ro>
2023-11-21 13:23:39 +0200

    fixed bugs

--- Project/Bulldozer.cpp
+++ Project/Bulldozer.cpp
@@ -3,21 +3,27 @@
 void twixt::Bulldozer::destoryRandomDot(Board& board)
 {
 	srand(time(NULL));
-	auto [i, j] = position;
-	i = rand() % (board.getSize() - 2) + 1;
-	j = rand() % (board.getSize() - 2) + 1;
+	int i = rand() % (board.getSize() - 2) + 1;
+	int j = rand() % (board.getSize() - 2) + 1;
 	while (board.getDot(i, j)->getStatus() != Dot::DotStatus::Player1 && board.getDot(i, j)->getStatus() != Dot::DotStatus::Player2)
 	{
 		i = rand() % (board.getSize() - 2) + 1;
 		j = rand() % (board.getSize() - 2) + 1;
 	}
+
+	//delete the random Dot
 	board.getDot(i, j)->setStatus(Dot::DotStatus::Bulldozer);
+	std::cout << "The dot " << i << " " << j << " will be destroyed\n";
+	board.getDot(i, j)->deleteAllBridgesForADot();
+
+	//eliminate the current bulldozer
 	board.getDot(position.first, position.second)->setStatus(Dot::DotStatus::Clear);
+
+	//set the new position for the bulldozer
 	position.first = i;
 	position.second = j;
-	std::cout << "The dot " << i << " " << j << "will be destroyed\n";
-	board.getDot(i, j)->deleteAllBridgesForADot();
-	std::cout << "Deleted.\n";
+
+	
 }
 
 twixt::Bulldozer::Bulldozer(Board& board)
@@ -26,23 +32,27 @@ twixt::Bulldozer::Bulldozer(Board& board)
 	auto [i, j] = position;
 	i = rand() % (board.getSize() - 2) + 1;
 	j = rand() % (board.getSize() - 2) + 1;
+
+	//find a clear Dot and set it as a bulldozer
 	while (board.getDot(i, j)->getStatus() != Dot::DotStatus::Clear)
 	{
 		i = rand() % (board.getSize() - 2) + 1;
 		j = rand() % (board.getSize() - 2) + 1;
 	}
 	board.changeDotStatus(i, j, Dot::DotStatus::Bulldozer);
+	position.first = i;
+	position.second = j;
 }
 
 void twixt::Bulldozer::flipCoin(Board& board)
 {
 	srand(time(NULL));
-	/*if (rand() % 2)
+	if (rand() % 2)
 	{
-		std::cout << "Nu se va intampla nimic\n";
+		std::cout << "We flipped the coin and nothing will happen.\n";
 		return;
-	}*/
-	std::cout << "vom distruge un dot\n";
+	}
+
 	destoryRandomDot(board);
 	board.showBoard();
 }

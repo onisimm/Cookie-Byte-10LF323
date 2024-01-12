b97a63c3349a58753054f544d2e647a3d6c73bd0
Onisim Roman <84021899+onisimm@users.noreply.github.com>
2024-01-12 13:23:36 +0200

    Add SwitchPlayer button implementation

--- app/GameScreenWidget.cpp
+++ app/GameScreenWidget.cpp
@@ -20,65 +20,6 @@ GameScreenWidget::~GameScreenWidget() {
     delete ui;
 }
 
-void GameScreenWidget::setupUIPlayers(const Ui::GameSettings& settings) {
-    auto setupPlayer = [this](Ui::UIPlayer& playerUI, QLabel* nameLabel, QLabel* timerLabel,
-        const QString& playerName, const uint16_t& timeLimit) {
-            playerUI.nameLabel = nameLabel;
-            playerUI.nameLabel->setText(playerName);
-
-            playerUI.timerLabel = timerLabel;
-            playerUI.timer = new QTimer(this);
-            playerUI.timer->setInterval(1000);
-            int minutes = timeLimit / 60;
-            int seconds = timeLimit % 60;
-            playerUI.timeLimit = timeLimit;
-            playerUI.timeLeft = timeLimit;
-            playerUI.timerLabel->setText(QString("%1:%2").arg(minutes, 2, 10, QChar('0')).arg(seconds, 2, 10, QChar('0')));
-
-            connect(playerUI.timer, &QTimer::timeout, this, [this, &playerUI]() { updateTimer(playerUI); });
-        };
-
-    setupPlayer(player1UI, ui->player1NameLabel, ui->player1TimerLabel, settings.player1Name, settings.timeLimit);
-
-    setupPlayer(player2UI, ui->player2NameLabel, ui->player2TimerLabel, settings.player2Name, settings.timeLimit);
-
-    initialPlayerFont = player1UI.nameLabel->font();
-}
-
-
-void GameScreenWidget::applyGameSettings(const Ui::GameSettings& settings) {
-    ui->mainLayout->addWidget(gameBoard);
-
-    setGameboardSize(settings.gameboardSize);
-    setMaxDots(settings.maxDots);
-    setMaxBridges(settings.maxBridges);
-    setGamemode(settings.gamemode);
-
-    gameBoard->buildBoard();
-}
-
-void GameScreenWidget::setupConnections()
-{
-    connect(ui->backToMenuButton, &QPushButton::clicked, this, &GameScreenWidget::on_backToMenuButton_clicked);
-    connect(gameBoard, &GameBoardWidget::dotPressed, this, &GameScreenWidget::handleDotPressed);
-    connect(ui->switchTurnButton, &QPushButton::clicked, this, &GameScreenWidget::switchPlayer);
-}
-
-void GameScreenWidget::updateUIBasedOnPlayerTurn() {}
-
-void GameScreenWidget::updateTimer(Ui::UIPlayer& player) {
-    if (player.timeLeft > 0) {
-        player.timeLeft--;
-        int minutes = player.timeLeft / 60;
-        int seconds = player.timeLeft % 60;
-        player.timerLabel->setText(QString("%1:%2").arg(minutes, 2, 10, QChar('0')).arg(seconds, 2, 10, QChar('0')));
-    }
-    else {
-        player.timer->stop();
-        // TODO: Handle time up situation
-    }
-}
-
 void GameScreenWidget::setGameboardSize(const uint8_t& size)
 {
     gameBoard->setGameboardSize(size);
@@ -102,12 +43,95 @@ void GameScreenWidget::setGamemode(const QString& gamemode)
     this->game->setGameMode(gamemode);
 }
 
+void GameScreenWidget::setupUIPlayers(const Ui::GameSettings& settings) {
+    auto setupPlayer = [this](Ui::UIPlayer& playerUI, QLabel* nameLabel, QLabel* timerLabel,
+        const QString& playerName, const uint16_t& timeLimit, const QColor& color) {
+            playerUI.nameLabel = nameLabel;
+            playerUI.nameLabel->setText(playerName);
+
+            playerUI.timerLabel = timerLabel;
+            playerUI.timer = new QTimer(this);
+            playerUI.timer->setInterval(1000);
+            int minutes = timeLimit / 60;
+            int seconds = timeLimit % 60;
+            playerUI.timeLimit = timeLimit;
+            playerUI.timeLeft = timeLimit;
+            playerUI.timerLabel->setText(QString("%1:%2").arg(minutes, 2, 10, QChar('0')).arg(seconds, 2, 10, QChar('0')));
+
+            playerUI.color = color;
+
+            QPalette palette;
+            palette.setColor(QPalette::WindowText, color);  // Set text color
+            playerUI.nameLabel->setPalette(palette);
+            playerUI.timerLabel->setPalette(palette);
+
+            connect(playerUI.timer, &QTimer::timeout, this, [this, &playerUI]() { updateTimer(playerUI); });
+        };
+
+    setupPlayer(player1UI, ui->player1NameLabel, ui->player1TimerLabel, settings.player1Name, settings.timeLimit, Qt::red);
+
+    setupPlayer(player2UI, ui->player2NameLabel, ui->player2TimerLabel, settings.player2Name, settings.timeLimit, Qt::black);
+
+    initialPlayerFont = player1UI.nameLabel->font();
+}
+
+
+void GameScreenWidget::applyGameSettings(const Ui::GameSettings& settings) {
+    ui->mainLayout->addWidget(gameBoard);
+
+    setGameboardSize(settings.gameboardSize);
+    setMaxDots(settings.maxDots);
+    setMaxBridges(settings.maxBridges);
+    setGamemode(settings.gamemode);
+
+    gameBoard->buildBoard();
+}
+
+void GameScreenWidget::setupConnections()
+{
+    connect(ui->backToMenuButton, &QPushButton::clicked, this, &GameScreenWidget::on_backToMenuButton_clicked);
+    connect(gameBoard, &GameBoardWidget::dotPressed, this, &GameScreenWidget::handleDotPressed);
+    connect(ui->switchTurnButton, &QPushButton::clicked, this, &GameScreenWidget::switchPlayer);
+}
+
+void GameScreenWidget::updateUIBasedOnPlayerTurn() {
+    Ui::UIPlayer& activePlayer = (currentPlayer == 1) ? player1UI : player2UI;
+    Ui::UIPlayer& inactivePlayer = (currentPlayer == 1) ? player2UI : player1UI;
+    
+    QFont font = initialPlayerFont;
+    font.setBold(true);
+    font.setPointSize(initialPlayerFont.pointSize() + 3);
+    activePlayer.nameLabel->setFont(font);
+    font.setPointSize(initialPlayerFont.pointSize() + 1);
+    activePlayer.timerLabel->setFont(font);
+    
+    font = initialPlayerFont;
+    inactivePlayer.nameLabel->setFont(initialPlayerFont);
+    font.setPointSize(initialPlayerFont.pointSize() - 2); // the timer label is smaller than the name label with 2pt always
+    inactivePlayer.timerLabel->setFont(initialPlayerFont);
+
+    activePlayer.timer->start();
+    inactivePlayer.timer->stop();
+}
+
+void GameScreenWidget::updateTimer(Ui::UIPlayer& player) {
+    if (player.timeLeft > 0) {
+        player.timeLeft--;
+        int minutes = player.timeLeft / 60;
+        int seconds = player.timeLeft % 60;
+        player.timerLabel->setText(QString("%1:%2").arg(minutes, 2, 10, QChar('0')).arg(seconds, 2, 10, QChar('0')));
+    }
+    else {
+        player.timer->stop();
+        // TODO: Handle time up situation
+    }
+}
+
 void GameScreenWidget::handleDotPressed(int row, int col) {
     gameBoard->setDotColor(row, col, Qt::red);
 }
 
 void GameScreenWidget::switchPlayer() {
-    // TOOD be able to press it only after a player has made a move
     currentPlayer = (currentPlayer == 1) ? 2 : 1;
     updateUIBasedOnPlayerTurn();
 }
